66a8e9d79fc64a6cc1ed1d1f2cdf975ddd43d826
ʙᴀᴅ ᴍᴜɴᴅᴀ 🌸 <154504921+Badhacker98@users.noreply.github.com>
2024-10-10 23:06:09 +0530

    Create config.py

---
+++ config.py
@@ -0,0 +1,16 @@
+from os import getenv
+
+from dotenv import load_dotenv
+
+load_dotenv()
+
+API_ID = "25742938"
+# -------------------------------------------------------------
+API_HASH = "b35b715fe8dc0a58e8048988286fc5b6"
+# --------------------------------------------------------------
+BOT_TOKEN = getenv("BOT_TOKEN", None)
+MONGO_URL = getenv("MONGO_URL", None)
+OWNER_ID = int(getenv("OWNER_ID", "7009601543"))
+SUPPORT_GRP = "PBX_CHAT"
+UPDATE_CHNL = "HEROKUBIN_01"
+OWNER_USERNAME = "ll_BAD_MUNDA_ll"
