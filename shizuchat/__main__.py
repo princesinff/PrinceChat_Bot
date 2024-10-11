ae64a3807e86dfdaad2282a5b0201125ce7c32d0
ʙᴀᴅ ᴍᴜɴᴅᴀ 🌸 <154504921+Badhacker98@users.noreply.github.com>
2024-10-11 23:32:17 +0530

    Update __main__.py

--- shizuchat/__main__.py
+++ shizuchat/__main__.py
@@ -3,8 +3,10 @@ import importlib
 
 from pyrogram import idle
 from config import OWNER_ID
+from pyrogram.types import BotCommand
 from shizuchat import LOGGER, shizuchat
 from shizuchat.modules import ALL_MODULES
+
 
 
 async def anony_boot():
@@ -18,14 +20,29 @@ async def anony_boot():
         importlib.import_module("shizuchat.modules." + all_module)
         LOGGER.info(f"Successfully imported : {all_module}")
 
+        # Set bot commands
+    try:
+        await nexichat.set_bot_commands(
+            commands=[
+                BotCommand("start", "sᴛᴀʀᴛ ᴛʜᴇ ʙᴏᴛ."),
+                BotCommand("help", "ɢᴇᴛ ᴛʜᴇ ʜᴇʟᴘ ᴍᴇɴᴜ."),
+                BotCommand("ping", "ᴄʜᴇᴄᴋ ɪғ ᴛʜᴇ ʙᴏᴛ ɪs ᴀʟɪᴠᴇ ᴏʀ ᴅᴇsᴅ."),
+            ]
+        )
+        LOGGER.info("Bot commands set successfully.")
+    except Exception as ex:
+        LOGGER.error(f"Failed to set bot commands: {ex}")
+        
     LOGGER.info(f"@{shizuchat.username} Started.")
     try:
         await shizuchat.send_message(int(OWNER_ID), f"{shizuchat.mention} has started")
     except Exception as ex:
         LOGGER.info(f"@{shizuchat.first_name} Started, please start the bot from owner id.")
+        
     await idle()
 
 
 if __name__ == "__main__":
     asyncio.get_event_loop().run_until_complete(anony_boot())
     LOGGER.info("Stopping shizuchat Bot...")
+    asyncio.get
