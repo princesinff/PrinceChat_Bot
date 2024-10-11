79c5622067b82c3bba13c8237133bdd72ca245c4
ʙᴀᴅ ᴍᴜɴᴅᴀ 🌸 <154504921+Badhacker98@users.noreply.github.com>
2024-10-11 15:27:42 +0530

    Create Chatgpt.py

---
+++ shizuchat/modules/Chatgpt.py
@@ -0,0 +1,33 @@
+import os
+import random
+import time
+from shizuchat import shizuchat
+from config import OWNER_USERNAME
+import requests
+from pyrogram.types import  Message
+from pyrogram import Client, filters
+from pyrogram.types import InputMediaPhoto
+from BadAPI import api
+from pyrogram.enums import ChatAction, ParseMode
+from pyrogram import filters
+
+
+@shizuchat.on_cmd(
+    filters.command(
+        ["chatgpt", "ai", "ask", "gpt", "solve"],
+        prefixes=["+", ".", "/", "-", "", "$", "#", "&"],
+    )
+)
+async def chat_gpt(bot, message):
+    
+    try:
+        await bot.send_chat_action(message.chat.id, ChatAction.TYPING)
+        if len(message.command) < 2:
+            await message.reply_text(
+            "Example:**\n\n/chatgpt Where is golden temple?")
+        else:
+            a = message.text.split(' ', 1)[1]
+            r=api.gemini(a)["results"]
+            await message.reply_text(f" {r} \n\n🌸  ᴍᴀᴅᴇ ᴡɪᴛʜ ❣️ ʙʏ [ʙᴀᴅ ᴍᴜɴᴅᴀ](https://t.me/{OWNER_USERNAME}) ||</b>", parse_mode=ParseMode.MARKDOWN)     
+    except Exception as e:
+        await message.reply_text(f"**ᴇʀʀᴏʀ: {e} ")
