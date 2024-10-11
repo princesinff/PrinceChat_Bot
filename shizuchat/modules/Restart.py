2f22e157d53294aab0e7737e4bb31e4d1ffb8796
ʙᴀᴅ ᴍᴜɴᴅᴀ 🌸 <154504921+Badhacker98@users.noreply.github.com>
2024-10-11 23:51:19 +0530

    Create Restart.py

---
+++ shizuchat/modules/Restart.py
@@ -0,0 +1,15 @@
+import os
+import shutil
+import asyncio
+from pyrogram.types import Message
+from pyrogram import filters, Client
+from shizuchat import shizuchat
+from config import OWNER_ID
+
+@shizuchat.on_cmd(["restart"]) & (filters.me | filters.user(OWNER_ID)))
+async def restart(client: Client, message: Message):
+    reply = await message.reply_text("**🔁 Rᴇsᴛᴀʀᴛɪɴɢ 🔥 ...**")
+    await message.delete()
+    await reply.edit_text("🥀 SᴜᴄᴄᴇssFᴜʟʟʏ RᴇSᴛᴀʀᴛᴇᴅ\n ︎ᴄʜᴀᴛʙᴏᴛ  🔥 ...\n\n💕 Pʟᴇᴀsᴇ Wᴀɪᴛ 1-2 MɪN Fᴏʀ\nLᴏᴀᴅ Usᴇʀ Pʟᴜɢɪɴs ✨ ...</b>")
+    os.system(f"kill -9 {os.getpid()} && python3 -m shizuchat")
+
