daccd794a1577fa29a2dd2b9017c38672d0292e1
ʙᴀᴅ ᴍᴜɴᴅᴀ 🌸 <154504921+Badhacker98@users.noreply.github.com>
2024-10-13 00:05:03 +0530

    Update update.py

--- shizuchat/modules/update.py
+++ shizuchat/modules/update.py
@@ -93,8 +93,7 @@ async def update(_, message: Message):
         )
     except:
         pass
-
-            return
+        return
         except Exception as err:
             await response.edit(
                 f"{nrs.text}\n\nsᴏᴍᴇᴛʜɪɴɢ ᴡᴇɴᴛ ᴡʀᴏɴɢ, ᴩʟᴇᴀsᴇ ᴄʜᴇᴄᴋ ʟᴏɢs."
