5153d4fef3d70598917efd308db64c234df8cb50
ʙᴀᴅ ᴍᴜɴᴅᴀ 🌸 <154504921+Badhacker98@users.noreply.github.com>
2024-10-12 19:15:33 +0530

    Create database.py

---
+++ shizuchat/database/database.py
@@ -0,0 +1,381 @@
+import json
+import os
+from typing import Dict, List, Union
+
+import config
+from shizuchat.database.mongo import mongodb
+
+channeldb = mongodb.cplaymode
+commanddb = mongodb.commands
+cleandb = mongodb.cleanmode
+playmodedb = mongodb.playmode
+playtypedb = mongodb.playtypedb
+langdb = mongodb.language
+authdb = mongodb.adminauth
+videodb = mongodb.badvideocalls
+onoffdb = mongodb.onoffper
+autoenddb = mongodb.autoend
+notesdb = mongodb.notes
+filtersdb = mongodb.filters
+
+# Shifting to memory [ mongo sucks often]
+loop = {}
+playtype = {}
+playmode = {}
+channelconnect = {}
+langm = {}
+pause = {}
+mute = {}
+active = []
+activevideo = []
+nonadmin = {}
+vlimit = []
+maintenance = []
+autoend = {}
+greeting_message = {"welcome": {}, "goodbye": {}}
+
+
+async def get_filters_count() -> dict:
+    chats_count = 0
+    filters_count = 0
+    async for chat in filtersdb.find({"chat_id": {"$lt": 0}}):
+        filters_name = await get_filters_names(chat["chat_id"])
+        filters_count += len(filters_name)
+        chats_count += 1
+    return {
+        "chats_count": chats_count,
+        "filters_count": filters_count,
+    }
+
+
+async def _get_filters(chat_id: int) -> Dict[str, int]:
+    _filters = await filtersdb.find_one({"chat_id": chat_id})
+    if not _filters:
+        return {}
+    return _filters["filters"]
+
+
+async def get_filters_names(chat_id: int) -> List[str]:
+    _filters = []
+    for _filter in await _get_filters(chat_id):
+        _filters.append(_filter)
+    return _filters
+
+
+async def get_filter(chat_id: int, name: str) -> Union[bool, dict]:
+    name = name.lower().strip()
+    _filters = await _get_filters(chat_id)
+    if name in _filters:
+        return _filters[name]
+    return False
+
+
+async def save_filter(chat_id: int, name: str, _filter: dict):
+    name = name.lower().strip()
+    _filters = await _get_filters(chat_id)
+    _filters[name] = _filter
+    await filtersdb.update_one(
+        {"chat_id": chat_id},
+        {"$set": {"filters": _filters}},
+        upsert=True,
+    )
+
+
+async def delete_filter(chat_id: int, name: str) -> bool:
+    filtersd = await _get_filters(chat_id)
+    name = name.lower().strip()
+    if name in filtersd:
+        del filtersd[name]
+        await filtersdb.update_one(
+            {"chat_id": chat_id},
+            {"$set": {"filters": filtersd}},
+            upsert=True,
+        )
+        return True
+    return False
+
+
+async def deleteall_filters(chat_id: int):
+    return await filtersdb.delete_one({"chat_id": chat_id})
+
+
+async def get_notes_count() -> dict:
+    chats_count = 0
+    notes_count = 0
+    async for chat in notesdb.find({"chat_id": {"$exists": 1}}):
+        notes_name = await get_note_names(chat["chat_id"])
+        notes_count += len(notes_name)
+        chats_count += 1
+    return {"chats_count": chats_count, "notes_count": notes_count}
+
+
+async def _get_notes(chat_id: int) -> Dict[str, int]:
+    _notes = await notesdb.find_one({"chat_id": chat_id})
+    if not _notes:
+        return {}
+    return _notes["notes"]
+
+
+async def get_note_names(chat_id: int) -> List[str]:
+    _notes = []
+    for note in await _get_notes(chat_id):
+        _notes.append(note)
+    return _notes
+
+
+async def get_note(chat_id: int, name: str) -> Union[bool, dict]:
+    name = name.lower().strip()
+    _notes = await _get_notes(chat_id)
+    if name in _notes:
+        return _notes[name]
+    return False
+
+
+async def save_note(chat_id: int, name: str, note: dict):
+    name = name.lower().strip()
+    _notes = await _get_notes(chat_id)
+    _notes[name] = note
+
+    await notesdb.update_one(
+        {"chat_id": chat_id}, {"$set": {"notes": _notes}}, upsert=True
+    )
+
+
+async def delete_note(chat_id: int, name: str) -> bool:
+    notesd = await _get_notes(chat_id)
+    name = name.lower().strip()
+    if name in notesd:
+        del notesd[name]
+        await notesdb.update_one(
+            {"chat_id": chat_id},
+            {"$set": {"notes": notesd}},
+            upsert=True,
+        )
+        return True
+    return False
+
+
+async def deleteall_notes(chat_id: int):
+    return await notesdb.delete_one({"chat_id": chat_id})
+
+
+async def set_private_note(chat_id, private_note):
+    await notesdb.update_one(
+        {"chat_id": chat_id}, {"$set": {"private_note": private_note}}, upsert=True
+    )
+
+
+async def is_pnote_on(chat_id) -> bool:
+    GetNoteData = await notesdb.find_one({"chat_id": chat_id})
+    if not GetNoteData == None:
+        if "private_note" in GetNoteData:
+            private_note = GetNoteData["private_note"]
+            return private_note
+        else:
+            return False
+    else:
+        return False
+
+
+# PLAY TYPE WHETHER ADMINS ONLY OR EVERYONE
+async def get_playtype(chat_id: int) -> str:
+    mode = playtype.get(chat_id)
+    if not mode:
+        mode = await playtypedb.find_one({"chat_id": chat_id})
+        if not mode:
+            playtype[chat_id] = "Everyone"
+            return "Everyone"
+        playtype[chat_id] = mode["mode"]
+        return mode["mode"]
+    return mode
+
+
+async def set_playtype(chat_id: int, mode: str):
+    playtype[chat_id] = mode
+    await playtypedb.update_one(
+        {"chat_id": chat_id}, {"$set": {"mode": mode}}, upsert=True
+    )
+
+
+
+# language
+async def get_lang(chat_id: int) -> str:
+    mode = langm.get(chat_id)
+    if not mode:
+        lang = await langdb.find_one({"chat_id": chat_id})
+        if not lang:
+            langm[chat_id] = "en"
+            return "en"
+        langm[chat_id] = lang["lang"]
+        return lang["lang"]
+    return mode
+
+
+async def set_lang(chat_id: int, lang: str):
+    langm[chat_id] = lang
+    await langdb.update_one({"chat_id": chat_id}, {"$set": {"lang": lang}}, upsert=True)
+
+
+# Muted
+async def is_muted(chat_id: int) -> bool:
+    mode = mute.get(chat_id)
+    if not mode:
+        return False
+    return mode
+
+
+async def mute_on(chat_id: int):
+    mute[chat_id] = True
+
+
+async def mute_off(chat_id: int):
+    mute[chat_id] = False
+
+
+# Delete command mode
+
+# Define file paths
+CLEANMODE_DB = os.path.join(config.TEMP_DB_FOLDER, "cleanmode.json")
+COMMAND_DB = os.path.join(config.TEMP_DB_FOLDER, "command.json")
+
+
+def load_cleanmode():
+    if os.path.exists(CLEANMODE_DB):
+        with open(CLEANMODE_DB, "r") as file:
+            return json.load(file)
+    return []
+
+
+def load_command():
+    if os.path.exists(COMMAND_DB):
+        with open(COMMAND_DB, "r") as file:
+            return json.load(file)
+    return []
+
+
+def save_cleanmode():
+    with open(CLEANMODE_DB, "w") as file:
+        json.dump(cleanmode, file)
+
+
+def save_command():
+    with open(COMMAND_DB, "w") as file:
+        json.dump(command, file)
+
+
+cleanmode = load_cleanmode()
+command = load_command()
+
+
+async def is_cleanmode_on(chat_id: int) -> bool:
+    return chat_id not in cleanmode
+
+
+async def cleanmode_off(chat_id: int):
+    if chat_id not in cleanmode:
+        cleanmode.append(chat_id)
+        save_cleanmode()
+
+
+async def cleanmode_on(chat_id: int):
+    if chat_id in cleanmode:
+        cleanmode.remove(chat_id)
+        save_cleanmode()
+
+
+async def is_commanddelete_on(chat_id: int) -> bool:
+    return chat_id not in command
+
+
+async def commanddelete_off(chat_id: int):
+    if chat_id not in command:
+        command.append(chat_id)
+        save_command()
+
+
+async def commanddelete_on(chat_id: int):
+    if chat_id in command:
+        command.remove(chat_id)
+        save_command()
+
+
+# Non Admin Chat
+async def check_nonadmin_chat(chat_id: int) -> bool:
+    user = await authdb.find_one({"chat_id": chat_id})
+    if not user:
+        return False
+    return True
+
+
+async def is_nonadmin_chat(chat_id: int) -> bool:
+    mode = nonadmin.get(chat_id)
+    if not mode:
+        user = await authdb.find_one({"chat_id": chat_id})
+        if not user:
+            nonadmin[chat_id] = False
+            return False
+        nonadmin[chat_id] = True
+        return True
+    return mode
+
+
+async def add_nonadmin_chat(chat_id: int):
+    nonadmin[chat_id] = True
+    is_admin = await check_nonadmin_chat(chat_id)
+    if is_admin:
+        return
+    return await authdb.insert_one({"chat_id": chat_id})
+
+
+async def remove_nonadmin_chat(chat_id: int):
+    nonadmin[chat_id] = False
+    is_admin = await check_nonadmin_chat(chat_id)
+    if not is_admin:
+        return
+    return await authdb.delete_one({"chat_id": chat_id})
+
+
+
+async def get_video_limit() -> str:
+    chat_id = 123456
+    if not vlimit:
+        dblimit = await videodb.find_one({"chat_id": chat_id})
+        if not dblimit:
+            limit = config.VIDEO_STREAM_LIMIT
+        else:
+            limit = dblimit["limit"]
+    else:
+        limit = vlimit[0]
+    return limit
+
+
+async def set_video_limit(limt: int):
+    chat_id = 123456
+    vlimit.clear()
+    vlimit.append(limt)
+    return await videodb.update_one(
+        {"chat_id": chat_id}, {"$set": {"limit": limt}}, upsert=True
+    )
+
+
+# On Off
+async def is_on_off(on_off: int) -> bool:
+    onoff = await onoffdb.find_one({"on_off": on_off})
+    if not onoff:
+        return False
+    return True
+
+
+async def add_on(on_off: int):
+    is_on = await is_on_off(on_off)
+    if is_on:
+        return
+    return await onoffdb.insert_one({"on_off": on_off})
+
+
+async def add_off(on_off: int):
+    is_off = await is_on_off(on_off)
+    if not is_off:
+        return
+    return await onoffdb.delete_one({"on_off": on_off})
+
