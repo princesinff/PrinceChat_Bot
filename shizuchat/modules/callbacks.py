4dbc0440a2d3f04214aa1b0b15edca38096d9d55
ʙᴀᴅ ᴍᴜɴᴅᴀ 🌸 <154504921+Badhacker98@users.noreply.github.com>
2024-10-13 22:32:22 +0530

    Create callbacks.py

---
+++ shizuchat/modules/callbacks.py
@@ -0,0 +1,36 @@
+import traceback
+from shizuchat import shizuchat
+from pyrogram import Client, filters
+from pyrogram.types import CallbackQuery, InlineKeyboardMarkup
+from shizuchat.modules.generate import generate_session, ask_ques, buttons_ques
+
+ERROR_MESSAGE = """ɪғ ʏᴏᴜ ᴀʀᴇ ɢᴇᴛᴛɪɴɢ ᴇʀʀᴏʀ!
+ʏᴏᴜ ʜᴀᴠᴇ ᴅᴏɴᴇ sᴏᴍᴇ ᴍɪsᴛᴀᴋᴇ ᴡʜɪʟᴇ ɢᴇɴᴇʀᴀᴛɪɴɢ.
+ɢɪᴠᴇɴ ᴡʀᴏɴɢ ᴅᴀᴛᴀ ᴏʀ ᴇʟsᴇ.
+ᴛʀʏ ᴀɢᴀɪɴ ɪғ ʏᴏᴜ ᴄᴀɴ.
+ᴏʀ ɪғ ʏᴏᴜ ʜᴀᴠᴇ ғɪʟʟᴇᴅ ᴛʜɪɴɢs ᴄᴏʀʀᴇᴄᴛʟʏ ʙᴜᴛ ɢᴇᴛᴛɪɴɢ ᴇʀʀᴏʀ,
+ᴛʜᴇɴ ғᴏʀᴡᴀʀᴅ ᴇʀʀᴏʀ ᴍsɢ ᴛᴏ [𝙹𝙰𝚁𝚅𝙸𝚂](https://t.me/JARVIS_V2) !"""
+
+@shizuchat.on_callback_query(filters.regex(pattern=r"^(generate|pyrogram|pyrogram_bot|telethon_bot|telethon)$"))
+async def _callbacks(bot: Client, callback_query: CallbackQuery):
+    query = callback_query.matches[0].group(1)
+    try:
+        if query == "generate":
+            await callback_query.answer()
+            await callback_query.message.reply(ask_ques, reply_markup=InlineKeyboardMarkup(buttons_ques))
+        elif query == "pyrogram":
+            await callback_query.answer()
+            await generate_session(bot, callback_query.message)
+        elif query == "pyrogram_bot":
+            await callback_query.answer("» ᴛʜᴇ sᴇssɪᴏɴ ɢᴇɴᴇʀᴀᴛᴇᴅ ᴡɪʟʟ ʙᴇ ᴏғ ᴩʏʀᴏɢʀᴀᴍ ᴠ2.", show_alert=True)
+            await generate_session(bot, callback_query.message, is_bot=True)
+        elif query == "telethon_bot":
+            await callback_query.answer()
+            await generate_session(bot, callback_query.message, telethon=True, is_bot=True)
+        elif query == "telethon":
+            await callback_query.answer()
+            await generate_session(bot, callback_query.message, telethon=True)
+    except Exception as e:
+        print(traceback.format_exc())
+        print(e)
+        await callback_query.message.reply(ERROR_MESSAGE.format(str(e)))
