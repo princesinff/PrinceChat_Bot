7c2038c6f01b02c44902fe6a26f22f98fd934d1b
ʙᴀᴅ ᴍᴜɴᴅᴀ 🌸 <154504921+Badhacker98@users.noreply.github.com>
2024-10-16 22:09:09 +0530

    Create Genn.py

---
+++ shizuchat/modules/Genn.py
@@ -0,0 +1,15 @@
+from pyrogram import filters
+from pyrogram.types import Message
+
+from shizuchat import shizuchat
+from shizuchat.modules.button import keyboard
+
+
+@shizuchat.on_cmd(["gen"])
+async def f_start(_, message: Message):
+    await message.reply_text(
+        text=f"ʜᴇʏ {message.from_user.first_name},\n\n๏ ɪ ᴀᴍ ᴛʀᴜꜱᴛᴇᴅ ꜱᴛʀɪɴɢ ɢᴇɴᴇʀᴀᴛᴏʀ ʙᴏᴛ ꜰᴜʟʟʏ ꜱᴀꜰᴇ & ꜱᴇᴄᴜʀᴇ, ᴡʀɪᴛᴛᴇɴ ɪɴ ᴩʏᴛʜᴏɴ ᴡɪᴛʜ ᴛʜᴇ ʜᴇʟᴩ ᴏғ ᴩʏʀᴏɢʀᴀᴍ.",
+        reply_markup=keyboard,
+        disable_web_page_preview=True,
+    )
+  
