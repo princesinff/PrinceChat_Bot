1742a92d2a50960b40053bbe4db8c0d1e41e9dfe
ʙᴀᴅ ᴍᴜɴᴅᴀ 🌸 <154504921+Badhacker98@users.noreply.github.com>
2024-10-17 07:36:53 +0530

    Update button.py

--- shizuchat/modules/helpers/button.py
+++ shizuchat/modules/helpers/button.py
@@ -2,6 +2,23 @@ from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup
 
 from config import SUPPORT_GRP
 
+# BUTTON
+
+retry_key = InlineKeyboardMarkup(
+    [[InlineKeyboardButton(text="ᴛʀʏ ᴀɢᴀɪɴ ☹️", callback_data="gensession")]]
+)
+
+gen_key = InlineKeyboardMarkup(
+    [
+        [
+            InlineKeyboardButton(text="ᴩʏʀᴏɢʀᴀᴍ v1 ♲︎︎︎", callback_data="pyrogram1"),
+            InlineKeyboardButton(text="ᴩʏʀᴏɢʀᴀᴍ v2 ♲︎︎︎", callback_data="pyrogram"),
+        ],
+        [InlineKeyboardButton(text="ᴛᴇʟᴇᴛʜᴏɴ ♲︎︎︎", callback_data="telethon")],
+    ]
+)
+
+#__________
 
 keyboard = InlineKeyboardMarkup(
     [
