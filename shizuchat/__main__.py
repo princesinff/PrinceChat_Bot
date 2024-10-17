9fac316de030f64aad6e48eef09127549d7bcc8f
ʙᴀᴅ ᴍᴜɴᴅᴀ 🌸 <154504921+Badhacker98@users.noreply.github.com>
2024-10-17 11:28:46 +0530

    Update __main__.py

--- shizuchat/__main__.py
+++ shizuchat/__main__.py
@@ -1,12 +1,13 @@
+import sys
 import asyncio
 import importlib
-
+from flask import Flask
+import threading
 from pyrogram import idle
-from config import OWNER_ID
 from pyrogram.types import BotCommand
+from config import OWNER_ID
 from shizuchat import LOGGER, shizuchat
 from shizuchat.modules import ALL_MODULES
-
 
 
 async def anony_boot():
@@ -14,13 +15,13 @@ async def anony_boot():
         await shizuchat.start()
     except Exception as ex:
         LOGGER.error(ex)
-        quit(1)
+        sys.exit(1)
 
     for all_module in ALL_MODULES:
         importlib.import_module("shizuchat.modules." + all_module)
-        LOGGER.info(f"Successfully imported : {all_module}")
+        LOGGER.info(f"ꜱᴜᴄᴄᴇꜱꜱꜰᴜʟʟʏ ɪᴍᴘᴏʀᴛᴇᴅ: {all_module}")
 
-        # Set bot commands
+    # Set bot commands
     try:
         await shizuchat.set_bot_commands(
             commands=[
@@ -29,20 +30,34 @@ async def anony_boot():
                 BotCommand("ping", "ᴄʜᴇᴄᴋ ɪғ ᴛʜᴇ ʙᴏᴛ ɪs ᴀʟɪᴠᴇ ᴏʀ ᴅᴇsᴅ."),
             ]
         )
-        LOGGER.info("Bot commands set successfully.")
+        LOGGER.info("ʙᴏᴛ ᴄᴏᴍᴍᴀɴᴅꜱ ꜱᴇᴛ ꜱᴜᴄᴄᴇꜱꜱꜰᴜʟʟʏ.")
     except Exception as ex:
-        LOGGER.error(f"Failed to set bot commands: {ex}")
-        
+        LOGGER.error(f"ꜰᴀɪʟᴇᴅ ᴛᴏ ꜱᴇᴛ ʙᴏᴛ ᴄᴏᴍᴍᴀɴᴅꜱ: {ex}")
+
     LOGGER.info(f"@{shizuchat.username} Started.")
     try:
         await shizuchat.send_message(int(OWNER_ID), f"{shizuchat.mention} has started")
     except Exception as ex:
-        LOGGER.info(f"@{shizuchat.first_name} Started, please start the bot from owner id.")
-        
+        LOGGER.info(f"@{shizuchat.username} ꜱᴛᴀʀᴛᴇᴅ, ᴘʟᴇᴀꜱᴇ ꜱᴛᴀʀᴛ ᴛʜᴇ ʙᴏᴛ ꜰʀᴏᴍ ᴏᴡɴᴇʀ ɪᴅ.")
+    
     await idle()
 
 
+# Flask Server Code for Health Check
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return "Bot is running"
+
+def run_flask():
+    app.run(host="0.0.0.0", port=8000)
+
 if __name__ == "__main__":
+    # Start Flask server in a new thread
+    flask_thread = threading.Thread(target=run_flask)
+    flask_thread.start()
+
+    # Start the bot asynchronously
     asyncio.get_event_loop().run_until_complete(anony_boot())
-    LOGGER.info("Stopping shizuchat Bot...")
-    asyncio.get
+    LOGGER.info("ꜱᴛᴏᴘᴘɪɴɢ ꜱʜɪᴢᴜᴄʜᴀᴛ ʙᴏᴛ...")
