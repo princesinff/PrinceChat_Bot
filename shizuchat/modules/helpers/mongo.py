aef3c0485a66973fa22fa7f4251907bbbd6917f8
ʙᴀᴅ ᴍᴜɴᴅᴀ 🌸 <154504921+Badhacker98@users.noreply.github.com>
2024-10-18 15:14:48 +0530

    Update mongo.py

--- shizuchat/modules/helpers/mongo.py
+++ shizuchat/modules/helpers/mongo.py
@@ -85,12 +85,12 @@ if MONGO_URL != None:
             return await modedb.insert_one({"chat_id": chat_id})
 
     def get_file_id(msg: Message):
-    if not msg.media: return None
-    for message_type in ("photo", "animation", "audio", "document", "video", "video_note", "voice", "sticker"):
-        obj = getattr(msg, message_type)
-        if obj:
-            setattr(obj, "message_type", message_type)
-            return obj
+        if not msg.media: return None
+            for message_type in ("photo", "animation", "audio", "document", "video", "video_note", "voice", "sticker"):
+                obj = getattr(msg, message_type)
+                if obj:
+                    setattr(obj, "message_type", message_type)
+                    return obj
 
     async def group_off():
         chat_id = 123
