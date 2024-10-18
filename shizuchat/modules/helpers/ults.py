22b98b6f3a47e2db674cd9b77608b7bd9b192a53
ʙᴀᴅ ᴍᴜɴᴅᴀ 🌸 <154504921+Badhacker98@users.noreply.github.com>
2024-10-18 15:18:00 +0530

    Create ults.py

---
+++ shizuchat/modules/helpers/ults.py
@@ -0,0 +1,8 @@
+def get_file_id(msg: Message):
+    if not msg.media: return None
+    for message_type in ("photo", "animation", "audio", "document", "video", "video_note", "voice", "sticker"):
+        obj = getattr(msg, message_type)
+        if obj:
+            setattr(obj, "message_type", message_type)
+            return obj
+          
