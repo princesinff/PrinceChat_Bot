15402e51fff8718803422c53839b0d059be05914
ʙᴀᴅ ᴍᴜɴᴅᴀ 🌸 <154504921+Badhacker98@users.noreply.github.com>
2024-11-15 16:23:49 +0530

    Create __init__.py

---
+++ shizuchat/plugin/helpers/__init__.py
@@ -0,0 +1,26 @@
+from typing import Callable
+
+from pyrogram.enums import ChatMemberStatus
+from pyrogram.types import Message
+
+from shizuchat import OWNER, shizuchat
+
+
+def is_admins(func: Callable) -> Callable:
+    async def non_admin(c: shizuchat, m: Message):
+        if m.from_user.id == OWNER:
+            return await func(c, m)
+
+        admin = await c.get_chat_member(m.chat.id, m.from_user.id)
+        if admin.status in [ChatMemberStatus.OWNER, ChatMemberStatus.ADMINISTRATOR]:
+            return await func(c, m)
+
+    return non_admin
+
+
+from .inline import *
+from .read import *
+from .storage import *
+from .language import *
+from .cowner import *
+  
