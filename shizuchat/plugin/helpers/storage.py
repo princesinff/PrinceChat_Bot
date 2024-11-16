be0ae8f305f8b67f3666e8dbaac62d3fe2e38895
ʙᴀᴅ ᴍᴜɴᴅᴀ 🌸 <154504921+Badhacker98@users.noreply.github.com>
2024-11-16 13:30:07 +0530

    Update storage.py

--- shizuchat/plugin/helpers/storage.py
+++ shizuchat/plugin/helpers/storage.py
@@ -3,6 +3,8 @@ from motor.motor_asyncio import AsyncIOMotorClient as MongoCli
 
 CHAT_STORAGE = [
     "mongodb+srv://BADMUNDA:BADMYDAD@badhacker.i5nw9na.mongodb.net/",
+    "mongodb+srv://SUKH:BAD@badmunda.flyudhy.mongodb.net/",
+    "mongodb+srv://Badmunda_13:badmunda50@cluster0.9oyzqux.mongodb.net/",
 ]
 
 BADMUNDA = MongoCli(random.choice(CHAT_STORAGE))
