270d521e7cd3229dba4b4662e7c2db45ea48585d
ʙᴀᴅ ᴍᴜɴᴅᴀ 🌸 <154504921+Badhacker98@users.noreply.github.com>
2024-11-16 14:19:53 +0530

    Update Editmsg.py

--- shizuchat/modules/Editmsg.py
+++ shizuchat/modules/Editmsg.py
@@ -9,4 +9,4 @@ async def delete_edited_message(client, message):
     # Wait for a specified time (e.g., 2 seconds)
     await asyncio.sleep(2)
     await message.delete()
-    await message.reply("ʏᴏᴜʀ ᴇᴅɪᴛᴇᴅ ᴍᴇssᴀɢᴇ ɪs ᴛᴏᴏ ʟᴏɴɢ ᴛʜᴀᴛ's ᴡʜʏ ɪ ʜᴀᴠᴇ ᴅᴇʟᴇᴛᴇᴅ ɪᴛ.")
+    await message.reply("**ʜᴇʏ, {message.from_user.mention}**\nʏᴏᴜʀ ᴇᴅɪᴛᴇᴅ ᴍᴇssᴀɢᴇ ɪs ᴛᴏᴏ ʟᴏɴɢ ᴛʜᴀᴛ's ᴡʜʏ ɪ ʜᴀᴠᴇ ᴅᴇʟᴇᴛᴇᴅ ɪᴛ.")
