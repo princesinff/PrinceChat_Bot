ec559690c1fcd3f58670b0455490204c7487f259
ʙᴀᴅ ᴍᴜɴᴅᴀ 🌸 <154504921+Badhacker98@users.noreply.github.com>
2024-11-16 11:42:55 +0530

    Update Editmsg.py

--- shizuchat/modules/Editmsg.py
+++ shizuchat/modules/Editmsg.py
@@ -2,7 +2,8 @@ from shizuchat import shizuchat
 from pyrogram import Client, filters
 import asyncio
 from pyrogram import *
-@shizuchat.on_message(filters.text)
+
+@shizuchat.on_edited_message(filters.group & ~filters.me)
 async def delete_edited_message(client, message):
     # Wait for a specified time (e.g., 10 seconds)
     await asyncio.sleep(10)
