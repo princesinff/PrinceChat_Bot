e4a974ffd667a8a204e0b48cb81603313240de9a
ʙᴀᴅ ᴍᴜɴᴅᴀ 🌸 <154504921+Badhacker98@users.noreply.github.com>
2024-11-17 17:40:07 +0530

    Update config.py

--- config/config.py
+++ config/config.py
@@ -10,7 +10,7 @@ API_ID = "25742938"
 API_HASH = "b35b715fe8dc0a58e8048988286fc5b6"
 # --------------------------------------------------------------
 BOT_TOKEN = getenv("BOT_TOKEN", None)
-STRING = getenv("STRING_SESSION", None)
+STRING1 = getenv("STRING_SESSION", None)
 DB_NAME = "shizuDB"
 MONGO_URL = getenv("MONGO_URL", None)
 OWNER_ID = int(getenv("OWNER_ID", "7009601543"))
