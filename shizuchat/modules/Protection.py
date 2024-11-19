939e73d378ff9d5f5d364379adb4d4af011e7f46
ʙᴀᴅ ᴍᴜɴᴅᴀ 🌸 <154504921+Badhacker98@users.noreply.github.com>
2024-11-19 23:34:58 +0530

    Update Protection.py

--- shizuchat/modules/Protection.py
+++ shizuchat/modules/Protection.py
@@ -5,8 +5,9 @@ from pyrogram.types import Message
 import asyncio
 from pyrogram import *
 
-
-@shizuchat.on_message(filters.text)  
-def delete_links(client, message):  
-    if "http://" in message.text or "https://" in message.text:  
-        client.delete_messages(message.chat.id, message.message_id)
+@shizuchat.on_message(filters.text)
+async def delete_links(client, message):
+    # Check if the message contains a link
+    if "http://" in message.text or "https://" in message.text:
+        # Delete the message
+        await client.delete_messages(message.chat.id, message.message_id)
