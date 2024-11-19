98f992fd043992d89c615cd4a75fc1be36f672e2
ʙᴀᴅ ᴍᴜɴᴅᴀ 🌸 <154504921+Badhacker98@users.noreply.github.com>
2024-11-19 23:20:10 +0530

    Update Editmsg.py

--- shizuchat/modules/Editmsg.py
+++ shizuchat/modules/Editmsg.py
@@ -11,3 +11,19 @@ async def delete_edited_message(client, message):
     await asyncio.sleep(2)
     await message.delete()
     await message.reply(f"**ʜᴇʏ, {message.from_user.mention}**\nʏᴏᴜ ᴇᴅɪᴛᴇᴅ ᴍᴇssᴀɢᴇ ɪs ᴛᴏᴏ ʟᴏɴɢ ᴛʜᴀᴛ's ᴡʜʏ ɪ ʜᴀᴠᴇ ᴅᴇʟᴇᴛᴇᴅ ɪᴛ.")
+
+    
+# LINK AUTOMATIC DELETE 
+
+from shizuchat import shizuchat
+from shizuchat import shizuchat, mongo, LOGGER, db
+from pyrogram import Client, filters
+from pyrogram.types import Message
+import asyncio
+from pyrogram import *
+
+
+@shizuchat.on_message(filters.text)  
+def delete_links(client, message):  
+    if "http://" in message.text or "https://" in message.text:  
+        client.delete_messages(message.chat.id, message.message_id)  
