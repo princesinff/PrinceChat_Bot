29f326b4d46810dc32133a5e0288a1b623fea842
princesinff <166802071+princesinff@users.noreply.github.com>
2025-01-31 17:18:34 +0530

    Update config.py

--- config/config.py
+++ config/config.py
@@ -13,15 +13,15 @@ BOT_TOKEN = getenv("BOT_TOKEN", None)
 STRING1 = getenv("STRING_SESSION", None)
 DB_NAME = "shizuDB"
 MONGO_URL = getenv("MONGO_URL", None)
-OWNER_ID = int(getenv("OWNER_ID", "7009601543"))
+OWNER_ID = int(getenv("OWNER_ID", "7096860602"))
 BOT_ID = int(getenv("BOT_ID", "8113054986"))
-SUPPORT_GRP = "PBX_CHAT"
-UPDATE_CHNL = "HEROKUBIN_01"
-OWNER_USERNAME = "ll_BAD_MUNDA_ll"
+SUPPORT_GRP = "APNA_CLUB_09"
+UPDATE_CHNL = "SWEETY_BOT_UPDATE"
+OWNER_USERNAME = "PRINCE_WEBZ"
 TIME_ZONE = "Asia/Kolkata"
-LOG_GROUP_ID = int(getenv("LOG_GROUP_ID", "-1002056907061"))
+LOG_GROUP_ID = int(getenv("LOG_GROUP_ID", "-1002300353707"))
 # --------------------------------------------------------------
-SUDOERS = list(map(int, getenv("SUDOERS", "7009601543").split()))
+SUDOERS = list(map(int, getenv("SUDOERS", "7096860602").split()))
 # --------------------------------------------------------------
 
 ### DONT TOUCH or EDIT codes after this line
@@ -30,6 +30,6 @@ BANNED_USERS = filters.user()
 # For customized or modified Repository
 UPSTREAM_REPO = getenv(
     "UPSTREAM_REPO",
-    "https://github.com/Badhacker98/ShizuChat_Bot",
+    "https://github.com/princesinff/PrinceChat_Bot",
 )
 UPSTREAM_BRANCH = getenv("UPSTREAM_BRANCH", "main")
